45155d9a9abe61ca7e8105f6bb4274abae189443
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:49:23 +0200

    Update: AUTOSAR basic software ECU abstraction module FrIf

--- LATEST/FrIf.cpp
+++ LATEST/FrIf.cpp
@@ -96,8 +96,14 @@ FUNC(void, FRIF_CODE) module_FrIf::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_FrIf as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == FrIf_InitCheck)

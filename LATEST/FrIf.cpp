4e3f729a6d48f416907f5f4a75e252d607733c3f
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:06 +0000

    Update: AUTOSAR basic software ECU abstraction module FrIf

--- LATEST/FrIf.cpp
+++ LATEST/FrIf.cpp
@@ -113,7 +113,7 @@ FUNC(void, FRIF_CODE) module_FrIf::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  FRIF_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, FRIF_CODE) module_FrIf::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  FRIF_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, FRIF_CODE) module_FrIf::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  FRIF_E_UNINIT
       );
 #endif
    }

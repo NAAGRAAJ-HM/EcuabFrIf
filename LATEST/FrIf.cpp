2be48bd066d852983508a6ad7a615482416cda85
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:34:56 +0000

    Update: AUTOSAR basic software ECU abstraction module FrIf

--- LATEST/FrIf.cpp
+++ LATEST/FrIf.cpp
@@ -35,6 +35,13 @@ class module_FrIf:
       public abstract_module
    ,  public class_FrIf_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_FrIf(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }

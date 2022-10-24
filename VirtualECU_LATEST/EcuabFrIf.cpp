0570c25576fbbb00786eb850c8cbed07ee379bad
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:04:46 +0200

    Update: AUTOSAR basic software ECU abstraction module for flexray communication protocol stack - interface layer

--- VirtualECU_LATEST/EcuabFrIf.cpp
+++ VirtualECU_LATEST/EcuabFrIf.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabFrIf.cpp                                                          */
+/* File   : EcuabFrIf.cpp                                                     */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABFRIF_AR_RELEASE_VERSION_MAJOR                                          4
-#define ECUABFRIF_AR_RELEASE_VERSION_MINOR                                          3
+#define ECUABFRIF_AR_RELEASE_VERSION_MAJOR                                     4
+#define ECUABFRIF_AR_RELEASE_VERSION_MINOR                                     3
 
 /******************************************************************************/
 /* MACROS                                                                     */
